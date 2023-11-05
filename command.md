d7c8fdab6a3e7acd3694ce55c41effbcee9e6e73
anthonynarine <anarine83@gmail.com>
2023-11-05 14:08:51 -0500

    task queue's tested and works as expected moving on to task grouping

--- command.md
+++ command.md
@@ -7,3 +7,19 @@ docker-compose down  << stops and removes containers
 docker exec -it django /bin/sh   << opens up an interactive shell 
 python manage.py shell
 
+
+
+
+
+tp1.delay()
+tp2.delay()
+tp3.delay()
+tp4.delay()
+tp1.delay()
+tp2.delay()
+tp3.delay()
+tp4.delay()
+tp1.delay()
+tp2.delay()
+tp3.delay()
+tp4.delay()
--- worker/tasks.py
+++ worker/tasks.py
@@ -1,11 +1,22 @@
 from celery import shared_task
-
+import time
 
 @shared_task
-def task1():
+def tp1 (queue="celery"):
+    time.sleep(3)
     return
 
 @shared_task
-def task2():
+def tp2 (queue="celery1"):
+    time.sleep(3)
     return
 
+@shared_task
+def tp3 (queue="celery2"):
+    time.sleep(3)
+    return
+
+@shared_task
+def tp4 (queue="celery3"):
+    time.sleep(3)
+    return
